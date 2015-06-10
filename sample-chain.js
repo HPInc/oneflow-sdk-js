0cc0dd556630cf3a1aa058bcf5ebf61fad500c33
Nigel Watson <nigel.watson@vaderfire.com>
2015-06-10 18:03:16 +0100

    updated with chain

---
+++ sample-chain.js
@@ -0,0 +1,36 @@
+var oneflowAPI = require("./lib/chain.js");
+
+var endpoint = "http://localhost:3000/api";
+var token = process.env.ONEFLOW_TOKEN;
+var secret = process.env.ONEFLOW_SECRET;
+var path = "business-cards.pdf";
+var fetch = true;
+var orderId = Math.ceil(Math.random()*1000000);
+
+oneflowAPI
+	.credentials(endpoint, token, secret)
+	.get("product")
+	.chain(function(products, next){
+		var pHash = {};
+		products.data.forEach(function(p){
+			pHash[p.productCode] = {
+				description:	p.description,
+				components:		[]
+			};
+			p.components.forEach(function(c){
+				pHash[p.productCode].components.push({
+					code:	c.code
+				});
+			});
+		});
+		// next(null, pHash);
+		next(null);
+	})
+	.output()
+	.createOrder("oneflow", orderId)
+	.addShipment("name", "companyName", "address1", "address2", "address3", "town", "state", "postcode", "isoCountry", "country", "email", "phone")
+	.addCarrier("royalmail", "firstclass")
+	.addItem("Business Cards 90x55", 1)
+	.addComponent("text", path, fetch)
+	.submit()
+	.output();
