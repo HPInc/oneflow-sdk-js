8203d66c12e7b3d2b01246cff15bda9bfb058f70
Nigel Watson <nigel.watson@vaderfire.com>
2015-06-11 15:34:25 +0100

    removed get

--- sample-chain.js
+++ sample-chain.js
@@ -9,24 +9,24 @@ var orderId = Math.ceil(Math.random()*1000000);
 
 oneflowAPI
 	.credentials(endpoint, token, secret)
-	.get("product")
-	.chain(function(products, next){
-		var pHash = {};
-		products.data.forEach(function(p){
-			pHash[p.productCode] = {
-				description:	p.description,
-				components:		[]
-			};
-			p.components.forEach(function(c){
-				pHash[p.productCode].components.push({
-					code:	c.code
-				});
-			});
-		});
-		// next(null, pHash);
-		next(null);
-	})
-	.output()
+	// .get("product")
+	// .chain(function(products, next){
+	// 	var pHash = {};
+	// 	products.data.forEach(function(p){
+	// 		pHash[p.productCode] = {
+	// 			description:	p.description,
+	// 			components:		[]
+	// 		};
+	// 		p.components.forEach(function(c){
+	// 			pHash[p.productCode].components.push({
+	// 				code:	c.code
+	// 			});
+	// 		});
+	// 	});
+	// 	// next(null, pHash);
+	// 	next(null);
+	// })
+	// .output()
 	.createOrder("oneflow", orderId)
 	.addShipment("name", "companyName", "address1", "address2", "address3", "town", "state", "postcode", "isoCountry", "country", "email", "phone")
 	.addCarrier("royalmail", "firstclass")
